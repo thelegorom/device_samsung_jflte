6c6e713a0014f0814ad75f9b638ddef556954bf9
nicklovell23 <nicklovell23@gmail.com>
2014-03-09 22:44:30 +0100

    Update dependenices
    Change-Id: I7077e360133268df088dbf51b1ac274874e241a9

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -31,10 +31,14 @@ BOARD_KERNEL_CMDLINE         := androidboot.hardware=qcom user_debug=31 zcache m
 BOARD_KERNEL_BASE            := 0x80200000
 BOARD_MKBOOTIMG_ARGS         := --ramdisk_offset 0x02000000
 BOARD_KERNEL_PAGESIZE        := 2048
-TARGET_KERNEL_VARIANT_CONFIG := cyanogen_jf_defconfig
+TARGET_KERNEL_VARIANT_CONFIG := KT_jf_defconfig
 TARGET_KERNEL_SELINUX_CONFIG := jfselinux_defconfig
 
 TARGET_BOOTLOADER_BOARD_NAME := MSM8960
+
+# linaro
+TARGET_KERNEL_CUSTOM_TOOLCHAIN := linaro-4.7-12.10/bin/arm-linux-gnueabihf-
+
 
 # WiFi module
 WIFI_DRIVER_MODULE_PATH := "/system/lib/modules/dhd.ko"
