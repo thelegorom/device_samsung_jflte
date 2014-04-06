fc13a17bb13d3cefe85d057290e73d6af2da5749
Lars Greiss <kufikugel@googlemail.com>
2014-04-07 00:56:36 +0200

    jflte: inherit multicarrier overlays
    Change-Id: I1d0f76751e165ce783c1c9900fe97551adbcc0d8

--- slim.mk
+++ slim.mk
@@ -3,7 +3,10 @@ $(call inherit-product, device/samsung/jflte/full_jflte.mk)
 # Enhanced NFC
 $(call inherit-product, vendor/slim/config/nfc_enhanced.mk)
 
-# Inherit some common CM stuff.
+# Inherit some common slim stuff.
 $(call inherit-product, vendor/slim/config/common_full_phone.mk)
+
+# Inherit multicarrier.
+$(call inherit-product, vendor/slim/config/common_multicarrier.mk)
 
 PRODUCT_NAME := slim_jflte
